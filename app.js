e9c754120a73631f45252adb086d94bc1e1abb5a
Audun Ytterdal <ay@vg.no>
2016-05-27 12:49:05 +0200

    Adding body-parser

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var app = express();
 
 // view engine setup
 //app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({type: "*/*"}));
-app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
