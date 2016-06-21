c267a971b57a84cb26674fd8ba60d1fd18aa3ea7
Audun Ytterdal <ay@vg.no>
2016-06-21 10:16:12 +0200

    Added syslog and some logging

--- routes/garage.js
+++ routes/garage.js
@@ -5,7 +5,7 @@ var request = require('request');
 var config = require('../config.json');
 var auth = "Basic " + new Buffer(config.username + ":" + config.password).toString("base64");
 var moment = require('moment');
-
+var syslog = require('modern-syslog');
 
 // Page
 router.post('/',  function(req, res) {
@@ -29,9 +29,9 @@ router.all('/api/v1/state:format?', function(req,res) {
 	console.log(req.body);
 	res.setHeader('Cache-Control', 'private, max-age=0');
 	if (req.method == "POST") {
-		console.log(req.body);
 		state.state = req.body.state;
 		state.timestamp = Date.now();
+		syslog.note("Sensor says: " + state.state)
 	}
 	state.updated = moment(state.timestamp).format();
 	state.updatedsince = moment(state.timestamp).fromNow();
@@ -58,7 +58,8 @@ router.all('/api/v1/snapshot:format?', function(req,res) {
 
 router.post('/api/v1/trigger:format?',  function(req, res) {
 	var gpio = require('pi-gpio');
-	 res.setHeader('Content-Type', 'application/json');
+	res.setHeader('Content-Type', 'application/json');
+	syslog.warn("Garagedoor is triggered");
 	gpio.open(12,'output', function(err) {
 		gpio.write(12,1, function(err) {
 			if(!err) {
